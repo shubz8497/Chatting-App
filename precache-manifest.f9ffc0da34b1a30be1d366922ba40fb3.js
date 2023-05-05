aff6dad0eb5eb4bf81ada8bccc9653bffca7aa1e
Shubham Ajay Yelange <shubham.yelange@gmail.com>
2023-05-05 11:40:28 +0530

    Updates

---
+++ precache-manifest.f9ffc0da34b1a30be1d366922ba40fb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473c47e5546d5c6f2dce3f72ef45c4e3",
+    "url": "/Chatting-App/index.html"
+  },
+  {
+    "revision": "b925da7eaab86f3508f2",
+    "url": "/Chatting-App/static/css/main.d194dd61.chunk.css"
+  },
+  {
+    "revision": "f2d190e0287084594307",
+    "url": "/Chatting-App/static/js/2.94b55387.chunk.js"
+  },
+  {
+    "revision": "edaf88c8aa6bedcdb624531641d638d8",
+    "url": "/Chatting-App/static/js/2.94b55387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b925da7eaab86f3508f2",
+    "url": "/Chatting-App/static/js/main.1e02a934.chunk.js"
+  },
+  {
+    "revision": "420f91510a26f8642d8e",
+    "url": "/Chatting-App/static/js/runtime-main.61f7e8f2.js"
+  }
+]);
